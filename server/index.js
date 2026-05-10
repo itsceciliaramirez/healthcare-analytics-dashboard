83d6562bf6ae1f0ef657dfac3a6008c87410547d
itsceciliaramirez <ramirez.ceci@outlook.com>
2026-05-10 01:46:40 -0500

    Fix Render port

--- server/index.js
+++ server/index.js
@@ -159,7 +159,7 @@ app.get("/appointment-trends", async (req, res) => {
   }
 });
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
